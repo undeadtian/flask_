60e4a690438155f0af9cba1d3bfa450738647a04
undeadtian <827038803@qq.com>
2021-04-14 16:15:58 +0800

    修改优化

--- cali_env.py
+++ cali_env.py
@@ -11,12 +11,28 @@ logger = Logger('DRSUEnv').getlog()
 
 
 class DRSUEnv(object):
-    def __init__(self, project_name, drsu_id, tar_dir, root_dir):
+    def __init__(self, project_name, drsu_id, tar_dir, root_dir,
+                 remote_host, remote_port, remote_path, username='broadxt', password='broadxt333', ):
+        """
+        从远端服务器下载标定数据的压缩文件，解压后存放到标定程序文件夹中的指定路径下
+        Args:
+            project_name:项目名称 e.g:xiaoshan
+            drsu_id:drsuid e.g:1
+            tar_dir: 本地用与临时存放压缩文件的文件夹地址
+            root_dir: 本地标定程序中存放标定数据的文件夹地址 一般位于标定文件下 data/calibration/fixed_cam_calib
+            remote_host: 服务器ip
+            remote_port: 服务器端口号
+            remote_path: 服务器压缩文件所在文件夹地址
+            username:
+            password:
+        """
         self.project_name = project_name
         self.drsu_id = drsu_id
-        self.tar_dir = self.check_dir(os.path.join(tar_dir, project_name))
+        self.remote_info = [remote_host, remote_port, username, password]
+        self.remote_path = remote_path
         self.root_dir = root_dir
         self.tar_name = 'drsu{}.tar.gz'.format(str(self.drsu_id).rjust(2, '0'))
+        self.tar_dir = self.check_dir(os.path.join(tar_dir, project_name))
         # self.ssh_session = BaseCommand(host, port, username, password)
         # self.ssh_session.reconnect()  # 建立连接
 
@@ -27,26 +43,19 @@ class DRSUEnv(object):
             logger.info('创建目录：{}'.format(file_dir))
         return file_dir
 
-    def down_load_cali_data(self, host, port, remote_path, username='broadxt', password='broadxt333', ):
+    def down_load_cali_data(self):
         """
         从服务器上下载压缩文件，download自动判断是否下载成功
         Args:
-            host:服务器ip
-            port:端口
-            remote_path:远端文件夹绝对路径
-            username:账号
-            password:密码
-
         Returns:是否下载成功
-
         """
-        ssh_session = BaseCommand(host, port, username, password)
+        ssh_session = BaseCommand(*self.remote_info)
         ssh_session.reconnect()
-        remote_path = os.path.join(remote_path, self.project_name, self.tar_name)
+        remote_path = os.path.join(self.remote_path, self.project_name, self.tar_name)
         locate_path = os.path.join(self.tar_dir, self.tar_name)
         try:
             ssh_session.download(remote_path, locate_path)
-            logger.info('压缩文件：{}下载到本地：{}成功'.format(remote_path,locate_path))
+            logger.info('压缩文件：{}下载到本地：{}成功'.format(remote_path, locate_path))
             return True
         except IOError:
             logger.error('压缩文件下载失败，请检查配置:\n remote_path:{},locate_path:{}'.format(remote_path, locate_path))
@@ -56,7 +65,6 @@ class DRSUEnv(object):
         """
         解压下载的压缩文件
         Returns:
-
         """
         tar_path = os.path.join(self.tar_dir, self.tar_name)
         command = 'tar -zvxf {} -C {}'.format(tar_path, os.path.join(self.root_dir, self.project_name))
@@ -73,7 +81,23 @@ class DRSUEnv(object):
         subprocess.run(command)
         logger.info('将文件:{}拷贝到:{}'.format(camera_conf_path, dest_conf_path))
 
+    def prepare_for_cali_env(self):
+        """
+        标定环境准备
+        Returns:
+        """
+        logger.info('开始准备本地标定环境，项目：{}，drsu:{}'.format(self.project_name, self.drsu_id))
+        if not self.down_load_cali_data():
+            logger.error('标定环境准备失败！')
+            return False
+        self.unzip_cali_data()
+        self.move_camera_conf()
+        logger.info('标定环境准备成功')
+        return True
+
 
 if __name__ == '__main__':
-    A = DRSUEnv('xiaoshan', 1, '/home/broadxt/wth', '/home/wth/fabu/calibration-master/data/calibration/fixed_cam_calib')
-    A.down_load_cali_data('172,16.4.112', 22, '/home/broadxt/xiaoshan/drsu_data')
+    A = DRSUEnv('xiaoshan', 1, '/home/broadxt/wth',
+                '/home/wth/fabu/calibration-master/data/calibration/fixed_cam_calib',
+                '172,16.4.112', 22, '/home/broadxt/xiaoshan/drsu_data')
+    A.prepare_for_cali_env()
--- common/single_ton.py
+++ common/single_ton.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/4/13 下午3:28
 # @Author  : wth
+from functools import wraps
+
 
 # 单例装饰器
-def singleton(cls, *args, **kw):
-    instances = {}
+def singleton(cls):
+    _instances = {}
 
-    def _singleton():
-        if cls not in instances:
-            instances[cls] = cls(*args, **kw)
-        return instances[cls]
+    @wraps(cls)
+    def _singleton(*args, **kargs):
+        if cls not in _instances:
+            _instances[cls] = cls(*args, **kargs)
+        return _instances[cls]
 
     return _singleton
